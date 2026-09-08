1730a35169c3d0a920f01c21f5ec4f5e44a80637
ChristianAndrion <cfa3@pct.edu>
2026-09-08 13:52:21 -0400

    Initial Player Setup

---
+++ Assets/Player/PlayerScripts/PlayerMovementScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private InputAction playerInput;
+    
+    public float playerSpeed = 100f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
