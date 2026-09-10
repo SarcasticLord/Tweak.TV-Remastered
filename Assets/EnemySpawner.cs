4b8135638f4f51af84b857c5fc91190d0f1e25a5
Jack High <79032468+SarcasticLord@users.noreply.github.com>
2026-09-10 15:16:21 -0400

    started enemy spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public Transform EnemyPoint;
+    public GameObject Enemy; 
+    public GameObject EnemyExists;
+
+    public bool facingRight = true;
+
+    public int enemyCount = 0;
+    public int maxEnemies = 10;
+
+    void Start()
+    {
+        StartCoroutine(EnemySpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator EnemySpawn() 
+    {
+        while (true)
+        {
+            if (enemyCount < maxEnemies)
+            {
+                EnemyExists = Instantiate(Enemy, EnemyPoint.position, facingRight ? EnemyPoint.rotation : Quaternion.Euler(-90, 0, 0));
+                enemyCount++;
+            
+            }
+            yield return new WaitForSeconds(500f);  
+        } 
+    }
+}
