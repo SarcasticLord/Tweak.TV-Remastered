3954182ff24c33e3a1740109f3a76ace3fad5172
Patrick454165 <pim1@pct.edu>
2026-09-10 12:30:20 -0400

    Added pickup and throw base function

---
+++ Assets/Script/PlayerStatusEffects.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerStatusEffects : MonoBehaviour
+{
+    public bool isTrapped; //Should prevent player from moving
+    public bool isPoisoned; //Should put a weird screen effect on that makes it hard to see
+    public bool drankCoffee; //Should allow player to dash
+    public bool isDead; //Should open game over screen
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
